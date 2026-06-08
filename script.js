3e69d7080559f4aa00b51eca4b10826c8e44f484
.Pedro Alves <pedroca.alves18@gmail.com>
2026-06-08 20:21:23 -0300

    Feature: Troca de cores de fundo do site

---
+++ script.js
@@ -0,0 +1,14 @@
+function trocarTema(tema){
+
+    if(tema === "padrao"){
+        document.body.style.background = "#0f1f22";
+    }
+
+    if(tema === "azul"){
+        document.body.style.background = "#0d1b2a";
+    }
+
+    if(tema === "roxo"){
+        document.body.style.background = "#1b1028";
+    }
+}
