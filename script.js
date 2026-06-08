695a3faf98fa0004d24bdd2d2edebec54eaff6e1
.Pedro Alves <pedroca.alves18@gmail.com>
2026-06-08 20:47:55 -0300

    Feature: Formulário com tratamento de erros

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 function trocarTema(tema){
 
     if(tema === "padrao"){
-        document.body.style.background = "#0f1f22";
+        document.body.style.background = "#0f1f22"
     }
 
     if(tema === "azul"){
-        document.body.style.background = "#0d1b2a";
+        document.body.style.background = "#0d1b2a"
     }
 
     if(tema === "roxo"){
-        document.body.style.background = "#1b1028";
+        document.body.style.background = "#1b1028"
     }
 }
+
+document.getElementById("formulario").addEventListener("submit",(e)=>{
+
+    e.preventDefault()
+
+    const nome = document.getElementById("nome").value
+
+    const email = document.getElementById("email").value
+
+    const empresa = document.getElementById("empresa").value
+
+    const area = document.getElementById("area").value
+
+    if(nome === "" || email === "" || empresa === "" || area === ""){
+        alert("Preencha todos os campos.")
+        return
+    }
+    alert("Solicitação enviada com sucesso!")
+
+})
